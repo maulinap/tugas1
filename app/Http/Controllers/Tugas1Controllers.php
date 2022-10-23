bddf613bd62c8ee42f588cd3be7a926b523c6ebb
maulinap <maulinaputri58@gmail.com>
2022-10-23 19:31:39 +0700

    Initial commit

---
+++ app/Http/Controllers/Tugas1Controllers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Tugas1Controllers extends Controller
+{
+    public function tugaspertama()
+    {
+        return view('tugas1');
+    }
+}
